3d722c51d5c2cb7f4bdd9206aee0b7ca5ff22512
sparshturkane <rajaturkane99@gmail.com>
2020-11-25 09:14:06 +0530

    models

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('list users');
+router.get('/', function (req, res, next) {
+  res.send('get users')
 });
 
-router.post('/register', function(req, res, next) {
+router.post('/register', function (req, res, next) {
   res.send('register');
 });
 
-router.post('/login', function(req, res, next) {
+router.post('/login', function (req, res, next) {
   res.send('login');
 });
 
