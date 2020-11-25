b26de9d7daab488eced02a533660703bfe460f26
sparshturkane <rajaturkane99@gmail.com>
2020-11-25 13:50:16 +0530

    register done

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,7 @@
 const User = require('../models/user')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 exports.list = (req, res, next) => {
     User.find()
@@ -7,16 +10,50 @@ exports.list = (req, res, next) => {
 }
 
 exports.register = (req, res, next) => {
-    const user = new User(req.body);
-    user.save()
-        .then(data => {
-            console.log('then');
-            console.log(data);
-            res.send(data);
+
+    // Check if user exists
+    User.findOne({ email: req.body.email })
+        .then((data) => {
+            if (data) {
+                // User already present
+                res.send({
+                    status: true,
+                    data: {},
+                    msg: "User with this email already present !"
+                })
+            } else {
+                // Unique user insert!
+                const hashPassword = bcrypt.hashSync(req.body.password, saltRounds);
+
+                const user = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hashPassword
+                });
+
+                user.save()
+                    .then(data => {
+
+                        res.send({
+                            status: true,
+                            data: {
+                                name: data.name,
+                                email: data.email
+                            },
+                            msg: "User added successfully"
+                        });
+                    })
+                    .catch(err => {
+                        res.send({
+                            status: false,
+                            error: err,
+                            msg: "User adding failed"
+                        })
+                    })
+            }
         })
-        .catch(err => {
-            console.log("catch");
-            res.send(err)
+        .catch((err) => {
+            res.send({ status: false, err, msg: "Find User failed" })
         })
 
 }
--- middlewares/request_validation.js
+++ middlewares/request_validation.js
@@ -1,6 +1,5 @@
 const { body, validationResult } = require('express-validator')
 exports.registerUserValidationRules = () => {
-    console.log("registerUserValidationRules");
     return [
         body('name', 'Invalid user name').exists().notEmpty(),
         body('email', 'Invalid email').exists().isEmail().notEmpty(),
@@ -9,7 +8,6 @@ exports.registerUserValidationRules = () => {
 }
 
 exports.validateRegisterUser = (req, res, next) => {
-    console.log("validateRegisterUser");
     const errors = validationResult(req)
     if (errors.isEmpty()) {
         return next()
@@ -18,6 +16,8 @@ exports.validateRegisterUser = (req, res, next) => {
     errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }))
 
     return res.status(422).json({
+        status: false,
         errors: extractedErrors,
+        msg: "Request Validation Failed"
     })
 }
